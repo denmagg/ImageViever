ab12fa87f330d4eead5d56126e193525b333a0ea
Denis <denmagg.work@gmail.com>
2021-10-25 16:44:40 +0300

    Add .gitignire file

---
+++ ImageViever/GalleryModule/Model/Image.swift
@@ -0,0 +1,9 @@
+//
+//  Image.swift
+//  ImageViever
+//
+//  Created by Denis Medvedev on 25/10/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import Foundation
---
+++ ImageViever/ModuleBuilder.swift
@@ -0,0 +1,9 @@
+//
+//  ModuleBuilder.swift
+//  ImageViever
+//
+//  Created by Denis Medvedev on 25/10/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import Foundation
