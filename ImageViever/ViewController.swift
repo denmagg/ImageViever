40c61c9e952231f4bc95e46681dbf5147e9d7566
Denis <denmagg.work@gmail.com>
2021-10-25 16:02:42 +0300

    Initial Commit

---
+++ ImageViever/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageViever
+//
+//  Created by Denis Medvedev on 25/10/2021.
+//  Copyright © 2021 Denis Medvedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
